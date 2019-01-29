46b49fcd7e5856314bf2c06b0e8eb492c6899ab5
Luiz Pereira <luizfper@gmail.com>
2019-01-29 18:20:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  list_foods = ["alface", "feijao","carne"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
